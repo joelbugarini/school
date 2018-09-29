c2f943a441804b2ca3bc90debe8d672cda28d901
Joel Bugarini <joelbugarini@Joels-MacBook-Pro.local>
2018-09-29 09:48:09 -0700

    Sumatoria

---
+++ prac017_Tabla7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace prac016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que imprime tablas de multiplicar");
+            Console.WriteLine("Ingrese la tabla que desea visualizar");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(x + " x " + i + " = "+ i*x);
+            }
+        }
+    }
+}
---
+++ prac018_Sumatoria/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que muestra una Sumatoria");
+            Console.WriteLine("Ingrese el limite superior");
+            int limite = Convert.ToInt16(Console.ReadLine());
+            int suma = 0;
+            if(limite > 1) {
+                Console.Write(" 𝚺  = ");
+                for(int x = 1;x <= limite; x += 1){
+                    suma += x;
+                    if(x != limite) Console.Write(x + " + ");
+                    else Console.Write(x + " = ");
+                }
+                Console.WriteLine(suma);
+            }
+            else Console.WriteLine("Limite demasiado pequeño");
+        }
+    }
+}
