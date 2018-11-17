adeda28bcc94681c9ad37a27a81caba9568a47db
Joel Bugarini <Joelbugarini@hotmail.com>
2018-11-17 12:51:06 -0700

    practica en clase

--- modulo_2/prac_003/Promedio.Designer.cs
+++ modulo_2/prac_003/Promedio.Designer.cs
@@ -32,7 +32,12 @@
             this.lbl_captura = new System.Windows.Forms.Label();
             this.btn_capturar = new System.Windows.Forms.Button();
             this.txt_numero = new System.Windows.Forms.TextBox();
-            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.lbl_Mayor = new System.Windows.Forms.Label();
+            this.btn_Reiniciar = new System.Windows.Forms.Button();
+            this.lbl_Matriz = new System.Windows.Forms.Label();
+            this.lbl_Menor = new System.Windows.Forms.Label();
+            this.lbl_Suma = new System.Windows.Forms.Label();
+            this.lbl_Promedio = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbl_descripcion
@@ -67,24 +72,75 @@
             // 
             this.txt_numero.Location = new System.Drawing.Point(44, 60);
             this.txt_numero.Name = "txt_numero";
-            this.txt_numero.Size = new System.Drawing.Size(100, 20);
+            this.txt_numero.Size = new System.Drawing.Size(165, 20);
             this.txt_numero.TabIndex = 3;
+            this.txt_numero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_numero_KeyPress);
             // 
-            // lbl_resultado
+            // lbl_Mayor
             // 
-            this.lbl_resultado.AutoSize = true;
-            this.lbl_resultado.Location = new System.Drawing.Point(56, 143);
-            this.lbl_resultado.Name = "lbl_resultado";
-            this.lbl_resultado.Size = new System.Drawing.Size(58, 13);
-            this.lbl_resultado.TabIndex = 4;
-            this.lbl_resultado.Text = "Resultado:";
+            this.lbl_Mayor.AutoSize = true;
+            this.lbl_Mayor.Location = new System.Drawing.Point(215, 63);
+            this.lbl_Mayor.Name = "lbl_Mayor";
+            this.lbl_Mayor.Size = new System.Drawing.Size(39, 13);
+            this.lbl_Mayor.TabIndex = 4;
+            this.lbl_Mayor.Text = "Mayor:";
+            // 
+            // btn_Reiniciar
+            // 
+            this.btn_Reiniciar.Location = new System.Drawing.Point(134, 86);
+            this.btn_Reiniciar.Name = "btn_Reiniciar";
+            this.btn_Reiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Reiniciar.TabIndex = 6;
+            this.btn_Reiniciar.Text = "Reiniciar";
+            this.btn_Reiniciar.UseVisualStyleBackColor = true;
+            this.btn_Reiniciar.Click += new System.EventHandler(this.btn_Reiniciar_Click);
+            // 
+            // lbl_Matriz
+            // 
+            this.lbl_Matriz.AutoSize = true;
+            this.lbl_Matriz.Location = new System.Drawing.Point(41, 134);
+            this.lbl_Matriz.Name = "lbl_Matriz";
+            this.lbl_Matriz.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Matriz.TabIndex = 0;
+            // 
+            // lbl_Menor
+            // 
+            this.lbl_Menor.AutoSize = true;
+            this.lbl_Menor.Location = new System.Drawing.Point(215, 83);
+            this.lbl_Menor.Name = "lbl_Menor";
+            this.lbl_Menor.Size = new System.Drawing.Size(40, 13);
+            this.lbl_Menor.TabIndex = 7;
+            this.lbl_Menor.Text = "Menor:";
+            // 
+            // lbl_Suma
+            // 
+            this.lbl_Suma.AutoSize = true;
+            this.lbl_Suma.Location = new System.Drawing.Point(215, 101);
+            this.lbl_Suma.Name = "lbl_Suma";
+            this.lbl_Suma.Size = new System.Drawing.Size(37, 13);
+            this.lbl_Suma.TabIndex = 8;
+            this.lbl_Suma.Text = "Suma:";
+            // 
+            // lbl_Promedio
+            // 
+            this.lbl_Promedio.AutoSize = true;
+            this.lbl_Promedio.Location = new System.Drawing.Point(215, 120);
+            this.lbl_Promedio.Name = "lbl_Promedio";
+            this.lbl_Promedio.Size = new System.Drawing.Size(54, 13);
+            this.lbl_Promedio.TabIndex = 9;
+            this.lbl_Promedio.Text = "Promedio:";
             // 
             // Promedio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
-            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.lbl_Promedio);
+            this.Controls.Add(this.lbl_Suma);
+            this.Controls.Add(this.lbl_Menor);
+            this.Controls.Add(this.lbl_Matriz);
+            this.Controls.Add(this.btn_Reiniciar);
+            this.Controls.Add(this.lbl_Mayor);
             this.Controls.Add(this.txt_numero);
             this.Controls.Add(this.btn_capturar);
             this.Controls.Add(this.lbl_captura);
@@ -102,6 +158,11 @@
         private System.Windows.Forms.Label lbl_captura;
         private System.Windows.Forms.Button btn_capturar;
         private System.Windows.Forms.TextBox txt_numero;
-        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.Label lbl_Mayor;
+        private System.Windows.Forms.Button btn_Reiniciar;
+        private System.Windows.Forms.Label lbl_Matriz;
+        private System.Windows.Forms.Label lbl_Menor;
+        private System.Windows.Forms.Label lbl_Suma;
+        private System.Windows.Forms.Label lbl_Promedio;
     }
 }
--- modulo_2/prac_003/Promedio.cs
+++ modulo_2/prac_003/Promedio.cs
@@ -22,16 +22,57 @@ namespace prac_003
 
         private void btn_capturar_Click(object sender, EventArgs e)
         {
-            
-            numeros[contador] = Convert.ToDouble(txt_numero.Text);
-            contador++;
-            lbl_captura.Text = "Ingrese el numero " + (contador + 1) + ":";
-            txt_numero.Clear();
-            txt_numero.Focus();
+            try
+            {
+                numeros[contador] = Convert.ToDouble(txt_numero.Text);
 
-            if (contador+1 == 15)
-                lbl_resultado.Text = "El promedio es: " + numeros.Average();
+                contador++;
+                lbl_captura.Text = "Ingrese el numero " + (contador + 1) + ":";
+                txt_numero.Clear();
+                txt_numero.Focus();
+
+
+                if (contador + 1 == 16)
+                {
+                    lbl_Mayor.Text = "Mayor: " + numeros.Max();
+                    lbl_Menor.Text = "Menor: " + numeros.Min();
+                    lbl_Suma.Text = "Suma: " + numeros.Sum();
+                    lbl_Promedio.Text = "Promedio: " + numeros.Average();
+                    txt_numero.ReadOnly = true;
+                    btn_capturar.Enabled = false;
+
+                    for (int n = 0; n < 15; n++)
+                    {
+                        lbl_Matriz.Text += "Matriz [" + n + "] = " + numeros[n] + Environment.NewLine;
+                    }
+                }
+            }
+            catch {
+                MessageBox.Show("Solo numero bebe de luz");
+                txt_numero.Text = "";
+                txt_numero.Focus();
+            }
             
+        }
+
+        private void btn_Reiniciar_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+            txt_numero.ReadOnly = false;
+            btn_capturar.Enabled = true;
+            numeros = new double[15];
+            lbl_Matriz.Text = "";
+            lbl_captura.Text = "Ingrese el numero 1:";
+            lbl_Mayor.Text = "Mayor: ";
+            lbl_Menor.Text = "Menor: ";
+            lbl_Suma.Text = "Suma: ";
+            lbl_Promedio.Text = "Promedio: ";
+        }
+
+        private void txt_numero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (contador < 15)
+                if (e.KeyChar == (char)Keys.Return) btn_capturar_Click(sender, e);
         }
     }
 }
